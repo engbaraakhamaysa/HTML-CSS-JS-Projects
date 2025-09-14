e93d5469a9eed78644e40d44812c964e9428bd6f
Baraa <engbaraakhamaysa@gmail.com>
2025-09-14 07:12:20 +0300

    Edit And Add dcumintion to Code

--- Project-02-To-Do-List/script.js
+++ Project-02-To-Do-List/script.js
@@ -5,37 +5,39 @@ function addTask() {
   if (inputBox.value === "") {
     alert("You Must Write Something!");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7"; // x icon
-    li.appendChild(span);
+    let li = document.createElement("li"); // Create new Elenmtnt li
+    li.innerHTML = inputBox.value; // Get The velu from input inside li innerHtml
+    listContainer.appendChild(li); // appendChild li insed ul
+    let span = document.createElement("span"); // to creat reomv task
+    span.innerHTML = "\u00d7"; // x icon ,can user remov the task
+    li.appendChild(span); //appendd child remov inside the li
   }
-  inputBox.value = "";
-  saveData();
+  inputBox.value = ""; // Clear The input after add the task
+  saveData(); // Sava all elemin insid the ul elemint in the local storge
 }
 
 listContainer.addEventListener(
   "click",
   function (e) {
     if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
+      //chiek the event form the li elemnet
+      e.target.classList.toggle("checked"); // this toggle he add(<li class="checked">Task1</li> = "cecked") & reomve (<li>Task1</li>)
       saveData();
     } else if (e.target.tagName === "SPAN") {
+      //chech the event form the span elemnt
       e.target.parentElement.remove();
       saveData();
     }
   },
-  false
+  true
 );
 
 function saveData() {
-  localStorage.setItem("data", listContainer.innerHTML);
+  localStorage.setItem("data", listContainer.innerHTML); //set Data in the localstorge
 }
 
 function showTask() {
-  listContainer.innerHTML = localStorage.getItem("data");
+  listContainer.innerHTML = localStorage.getItem("data"); //get data in the localStorge
 }
 
-showTask();
+showTask(); // get data insed the local sorge ex: data:<li class="checked">task 1<span>×</span></li>
